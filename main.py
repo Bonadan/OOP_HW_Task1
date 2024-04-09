7ab529094d606e6c6e4365dd7291d8a4de894909
Bonadan <D-a-n-e@email.cz>
2024-04-09 16:25:52 +0200

    Code skelet

---
+++ main.py
@@ -0,0 +1,15 @@
+class Device:
+    def __init__(self, price, model, color):
+        self.price = price
+        self.model = model
+        self.color = color
+
+class CoffeMachine(Device):
+    pass
+
+class Blender(Device):
+    pass
+
+class MeatGrinder(Device):
+    pass
+
